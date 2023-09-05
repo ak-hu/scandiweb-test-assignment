3f44eea3fe0e2adc02f84d0830dbbfce59454bd1
Artem <git.ak.hu@gmail.com>
2023-09-06 01:17:43 +0200

    Update AddProduct.php

--- server/AddProduct.php
+++ server/AddProduct.php
@@ -7,31 +7,31 @@ class AddProduct {
     }
 
     // Метод для отображения формы добавления продукта и обработки данных из формы
-    public function render() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $sku = $_POST['sku'];
-            $name = $_POST['name'];
-            $price = $_POST['price'];
+    // public function render() {
+    //     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //         $sku = $_POST['sku'];
+    //         $name = $_POST['name'];
+    //         $price = $_POST['price'];
 
-            $this->product->setSku($sku);
-            $this->product->setName($name);
-            $this->product->setPrice($price);
+    //         $this->product->setSku($sku);
+    //         $this->product->setName($name);
+    //         $this->product->setPrice($price);
 
-            if ($this->product->save()) {
-                echo "Product added successfully!";
-            } else {
-                echo "Failed to add product.";
-            }
-        } else {
-            // Выводите форму для добавления продукта
-            echo "<h1>Add Product</h1>";
-            echo "<form method='post'>";
-            echo "SKU: <input type='text' name='sku'><br>";
-            echo "Name: <input type='text' name='name'><br>";
-            echo "Price: <input type='text' name='price'><br>";
-            echo "<input type='submit' value='Add Product'>";
-            echo "</form>";
-        }
-    }
+    //         if ($this->product->save()) {
+    //             echo "Product added successfully!";
+    //         } else {
+    //             echo "Failed to add product.";
+    //         }
+    //     } else {
+    //         // Выводите форму для добавления продукта
+    //         echo "<h1>Add Product</h1>";
+    //         echo "<form method='post'>";
+    //         echo "SKU: <input type='text' name='sku'><br>";
+    //         echo "Name: <input type='text' name='name'><br>";
+    //         echo "Price: <input type='text' name='price'><br>";
+    //         echo "<input type='submit' value='Add Product'>";
+    //         echo "</form>";
+    //     }
+    // }
 }
 ?>
