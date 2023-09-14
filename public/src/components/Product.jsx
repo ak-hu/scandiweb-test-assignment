72d2a59a5f96ad930ef434c2d526bdf7df4ae7d1
Artem <git.ak.hu@gmail.com>
2023-09-15 01:08:49 +0200

    mass delete button functionality on client side

--- public/src/components/Product.jsx
+++ public/src/components/Product.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Product = ({ product }) => {
+const Product = ({ product, onCheckboxChange }) => {
+    const handleCheckboxChange = (event) => {
+        const productId = event.target.id;
+        onCheckboxChange(productId, event.target.checked);
+    }
+    
     return (
         <div className='product'>
-            <input type="checkbox" name="" id="" className='delete-checkbox'/>
+            <input type="checkbox" name="" id={product.id} className='delete-checkbox'
+            onChange={handleCheckboxChange} />
             <span>{ product.sku }</span>
             <span>{ product.name }</span>
             <span>{ product.price } $</span>
--- public/src/pages/ProductList.jsx
+++ public/src/pages/ProductList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 import Footer from '../components/Footer';
 import Product from '../components/Product';
@@ -8,39 +9,57 @@ import Product from '../components/Product';
 const ProductList = () => {
     const navigate = useNavigate();
     const [products, setProducts] = useState([]);
-    const addProduct = () => {
-        navigate('/add-product')
-    }
-    
-    useEffect(() => {
-        fetch('http://localhost:8888/scandidev/server/api.php?endpoint=getProducts')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                setProducts(data);
-            })
-            .catch(error => {
-                console.error('Fetch error:', error);
-            });
-    }, []);
+    const [selectedProducts, setSelectedProducts] = useState([]);
 
-    console.log(products)
+    const handleMassDelete = () => {
+        axios.post('http://localhost:8888/scandidev/server/api.php?endpoint=massDeleteProducts', {
+            productIds: selectedProducts,
+        })
+        .then(response => {
+            if (response.status === 200) {
+                const updatedProducts = products.filter(product => !selectedProducts.includes(product.id));
+                setProducts(updatedProducts);
+                setSelectedProducts([]);
+            }
+        })
+        .catch(error => {
+            console.error('Axios error:', error);
+        });
+    }
+
+    const handleCheckboxChange = (productId, isChecked) => {
+        if (isChecked) {
+            setSelectedProducts([...selectedProducts, productId]);
+        } else {
+            setSelectedProducts(selectedProducts.filter(id => id !== productId));
+        }
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost:8888/scandidev/server/api.php?endpoint=getProducts')
+        .then(response => {
+            if (response.status === 200) {
+                setProducts(response.data);
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        })
+        .catch(error => {
+            console.error('Axios error:', error);
+        });
+    }, []);
 
     return (
         <div className='container'>
             <div className='header'>
                 <h1>Product List</h1>
                 <div className='header--buttons'>
-                    <button onClick={() => addProduct()}>add</button>
-                    <button>mass delete</button>
+                    <button onClick={() => navigate('/add-product')}>add</button>
+                    <button onClick={handleMassDelete}>mass delete</button>
                 </div>
             </div>
             <div className='content products-container'>
-                {products.map((product) => <Product key={product.id} product={product} />
+                {products.map((product) => <Product key={product.id} product={product} onCheckboxChange={handleCheckboxChange}/>
                 )}
             </div>
             <Footer />
